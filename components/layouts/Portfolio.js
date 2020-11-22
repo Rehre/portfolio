dd42d0abbc374e51d6481dfcb41c95fd3741bfe3
Rehre <akmalazzam2240@gmail.com>
2020-11-22 14:21:28 +0700

    Update | adding page layout and profile page

---
+++ components/layouts/Portfolio.js
@@ -0,0 +1,9 @@
+import Title from "../commons/Title";
+
+export default function Portfolio() {
+  return (
+    <div className="w-full h-screen">
+      <Title text="My Projects" />
+    </div>
+  );
+}
